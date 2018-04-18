a547601c1cfd370350689e33a7e8ab5bf032709c
root <root@brayet-pc.salgados>
2018-04-18 15:00:47 -0600

    final 2

--- DEPLOY/.terraform/modules/37f14e5c626a9ebe13125c0a69a7a875/terraform-aws-modules-terraform-aws-elb-0bf0d84/modules/elb_attachment/main.tf
+++
@@ -1,9 +0,0 @@
-#################
-# ELB attachment
-#################
-resource "aws_elb_attachment" "this" {
-  count = "${var.number_of_instances}"
-
-  elb      = "${var.elb}"
-  instance = "${element(var.instances, count.index)}"
-}
